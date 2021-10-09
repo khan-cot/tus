2ce42ff988cd496a073e3a01b31c8df4b2868ead
khan-cot <>
2021-10-09 10:00:07 +0800

    init contract

---
+++ TusswapRouter.sol
@@ -0,0 +1,198 @@
+pragma solidity >=0.6.12;
+
+
+import './libraries/TusswapLibrary.sol';
+import './libraries/SafeMath.sol';
+import './libraries/TransferHelper.sol';
+import './interfaces/ITusswapFactory.sol';
+import './interfaces/IERC20.sol';
+
+
+contract TusswapRouter {
+    using SafeMathTusswap for uint;
+
+    address public immutable  factory;
+    address public immutable  WTUS;
+
+    modifier ensure(uint deadline) {
+        require(deadline >= block.timestamp, 'TusswapRouter: EXPIRED');
+        _;
+    }
+
+    constructor(address _factory, address _WTUS) public {
+        factory = _factory;
+        WTUS = _WTUS;
+    }
+
+    // **** ADD LIQUIDITY ****
+    function _addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin
+    ) internal virtual returns (uint amountA, uint amountB) {
+        // create the pair if it doesn't exist yet
+        if (ITusswapFactory(factory).getPair(tokenA, tokenB) == address(0)) {
+            ITusswapFactory(factory).createPair(tokenA, tokenB);
+        }
+        (uint reserveA, uint reserveB, ) = getReserves(factory, tokenA, tokenB);
+        if (reserveA == 0 && reserveB == 0) {
+            (amountA, amountB) = (amountADesired, amountBDesired);
+        } else {
+            uint amountBOptimal = TusswapLibrary.quote(amountADesired, reserveA, reserveB);
+            if (amountBOptimal <= amountBDesired) {
+                require(amountBOptimal >= amountBMin, 'TusswapRouter: INSUFFICIENT_B_AMOUNT');
+                (amountA, amountB) = (amountADesired, amountBOptimal);
+            } else {
+                uint amountAOptimal = TusswapLibrary.quote(amountBDesired, reserveB, reserveA);
+                assert(amountAOptimal <= amountADesired);
+                require(amountAOptimal >= amountAMin, 'TusswapRouter: INSUFFICIENT_A_AMOUNT');
+                (amountA, amountB) = (amountAOptimal, amountBDesired);
+            }
+        }
+    }
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external virtual ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
+        require(tokenB == WTUS, 'TusswapRouter: only tus address');
+        (amountA, amountB) = _addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+        address pair = TusswapLibrary.pairFor(factory, tokenA, tokenB);
+        TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        uint mint0 = amountBDesired;
+        liquidity = ITusswapPair(pair).mint(to,mint0);
+    }
+    // **** REMOVE LIQUIDITY ****
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) public virtual ensure(deadline) returns (uint amountA, uint amountB) {
+        address pair = TusswapLibrary.pairFor(factory, tokenA, tokenB);
+        ITusswapPair(pair).transferFrom(msg.sender, pair, liquidity); // send liquidity to pair
+        (uint amount0, uint amount1) = ITusswapPair(pair).burn(to);
+        (address token0,) = TusswapLibrary.sortTokens(tokenA, tokenB);
+        (amountA, amountB) = tokenA == token0 ? (amount0, amount1) : (amount1, amount0);
+        require(amountA >= amountAMin, 'TusswapRouter: INSUFFICIENT_A_AMOUNT');
+        require(amountB >= amountBMin, 'TusswapRouter: INSUFFICIENT_B_AMOUNT');
+    }
+
+    // **** SWAP ****
+    // requires the initial amount to have already been sent to the first pair
+    function _swap(uint[] memory amounts, address[] memory path, address _to) internal virtual {
+        for (uint i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (address token0,) = TusswapLibrary.sortTokens(input, output);
+            uint amountOut = amounts[i + 1];
+            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
+            address to = i < path.length - 2 ? TusswapLibrary.pairFor(factory, output, path[i + 2]) : _to;
+            ITusswapPair(TusswapLibrary.pairFor(factory, input, output)).swap(
+                amount0Out, amount1Out, to
+            );
+        }
+    }
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external virtual ensure(deadline) returns (uint[] memory amounts) {
+        amounts = getAmountsOut(factory, amountIn, path);
+        require(amounts[amounts.length - 1] >= amountOutMin, 'TusswapRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, TusswapLibrary.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, to);
+    }
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external virtual ensure(deadline) returns (uint[] memory amounts) {
+        amounts = getAmountsIn(factory, amountOut, path);
+        require(amounts[0] <= amountInMax, 'TusswapRouter: EXCESSIVE_INPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, TusswapLibrary.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, to);
+    }
+
+    // **** LIBRARY FUNCTIONS ****
+    function pairFor(address factory, address tokenA, address tokenB) internal view returns (address pair) {
+        (address token0, address token1) = TusswapLibrary.sortTokens(tokenA, tokenB);
+        return ITusswapFactory(factory).getPair(token0, token1);
+    }
+    function getReserves(address factory, address tokenA, address tokenB) internal view returns (uint reserveA, uint reserveB, uint8 fee) {
+        (address token0,) = TusswapLibrary.sortTokens(tokenA, tokenB);
+        (uint reserve0, uint reserve1,) = ITusswapPair(pairFor(factory, tokenA, tokenB)).getReserves();
+        (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+        fee = ITusswapPair(pairFor(factory, tokenA, tokenB)).fee();
+    }
+    function quote(uint amountA, uint reserveA, uint reserveB) public pure virtual returns (uint amountB) {
+        return TusswapLibrary.quote(amountA, reserveA, reserveB);
+    }
+
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut, uint8 fee)
+        public
+        pure
+        virtual
+        returns (uint amountOut)
+    {
+        return TusswapLibrary.getAmountOut(amountIn, reserveIn, reserveOut, fee);
+    }
+
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut, uint8 fee)
+        public
+        pure
+        virtual
+        returns (uint amountIn)
+    {
+        return TusswapLibrary.getAmountIn(amountOut, reserveIn, reserveOut, fee);
+    }
+
+    function getAmountsOut(uint amountIn, address[] memory path)
+        public
+        view
+        virtual
+        returns (uint[] memory amounts)
+    {
+        require(path.length >= 2, 'TusswapRouter: INVALID_PATH');
+        amounts = new uint[](path.length);
+        amounts[0] = amountIn;
+        for (uint i; i < path.length - 1; i++) {
+            (uint reserveIn, uint reserveOut, uint8 fee) = getReserves(factory, path[i], path[i + 1]);
+            amounts[i + 1] = getAmountOut(amounts[i], reserveIn, reserveOut, fee);
+        }
+    }
+
+    function getAmountsIn(uint amountOut, address[] memory path)
+        public
+        view
+        virtual
+        returns (uint[] memory amounts)
+    {
+        require(path.length >= 2, 'TusswapRouter: INVALID_PATH');
+        amounts = new uint[](path.length);
+        amounts[amounts.length - 1] = amountOut;
+        for (uint i = path.length - 1; i > 0; i--) {
+            (uint reserveIn, uint reserveOut, uint8 fee) = getReserves(factory, path[i - 1], path[i]);
+            amounts[i - 1] = getAmountIn(amounts[i], reserveIn, reserveOut, fee);
+        }
+    }
+}
---
+++ libraries/TusswapLibrary.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.5.0;
+
+import '../interfaces/ITusswapPair.sol';
+
+import "./SafeMath.sol";
+
+library TusswapLibrary {
+    using SafeMathTusswap for uint;
+
+    // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, 'TusswapLibrary: IDENTICAL_ADDRESSES');
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'TusswapLibrary: ZERO_ADDRESS');
+    }
+    // given some amount of an asset and pair reserves, returns an equivalent amount of the other asset
+    function quote(uint amountA, uint reserveA, uint reserveB) internal pure returns (uint amountB) {
+        require(amountA > 0, 'TusswapLibrary: INSUFFICIENT_AMOUNT');
+        require(reserveA > 0 && reserveB > 0, 'TusswapLibrary: INSUFFICIENT_LIQUIDITY');
+        amountB = amountA.mul(reserveB) / reserveA;
+    }
+
+    // given an input amount of an asset and pair reserves, returns the maximum output amount of the other asset
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut, uint8 fee) internal pure returns (uint amountOut) {
+        require(amountIn > 0, 'TusswapLibrary: INSUFFICIENT_INPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'TusswapLibrary: INSUFFICIENT_LIQUIDITY');
+        uint amountInWithFee = amountIn.mul(1000-fee);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        amountOut = numerator / denominator;
+    }
+
+    // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut, uint8 fee) internal pure returns (uint amountIn) {
+        require(amountOut > 0, 'TusswapLibrary: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'TusswapLibrary: INSUFFICIENT_LIQUIDITY');
+        uint numerator = reserveIn.mul(amountOut).mul(1000);
+        uint denominator = reserveOut.sub(amountOut).mul(1000-fee);
+        amountIn = (numerator / denominator).add(1);
+    }
+
+}
